7e14cd2b9b8ef052427396a3965484e32677e5c1
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-10 23:22:48 +0200

    Add `ImageFlickController` view controller

---
+++ Dynamics/Source/ImageFlickController.swift
@@ -0,0 +1,11 @@
+//
+//  ImageFlickController.swift
+//  Dynamics
+//
+//  Created by Julian Grosshauser on 10/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class ImageFlickController: UIViewController {}
