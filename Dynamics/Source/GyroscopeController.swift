a17af608f37173fb3fcf904cebef3e348bf35b7b
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-13 23:48:35 +0200

    Add `GyroscopeController` view controller

---
+++ Dynamics/Source/GyroscopeController.swift
@@ -0,0 +1,11 @@
+//
+//  GyroscopeController.swift
+//  Dynamics
+//
+//  Created by Julian Grosshauser on 13/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class GyroscopeController: UIViewController {}
